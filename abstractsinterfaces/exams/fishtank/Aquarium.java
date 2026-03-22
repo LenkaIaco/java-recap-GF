f19e5af93400bd0a20b4f942839f8fa6b3ba7a71
LenkaIaco <lenkadan@gmail.com>
2026-03-22 09:30:06 +0100

    exam stage 1 recap

---
+++ abstractsinterfaces/exams/fishtank/Aquarium.java
@@ -0,0 +1,44 @@
+package exams.fishtank;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Aquarium {
+    List<Fish> fishes;
+
+    public Aquarium(){
+        fishes = new ArrayList<>();
+    }
+
+    public Aquarium(List<Fish> fishes){
+        if(fishes==null){fishes=new ArrayList<>();}
+        for (Fish f: fishes){
+            fishes.add(f);
+        }
+    }
+
+    public void addFish(Fish fish){
+        fishes.add(fish);
+    }
+    public void feed(){
+        for (Fish f: fishes){
+            f.feed();
+        }
+    }
+
+    public void removeFish(){
+        for (Fish f: fishes){
+            if (f.getWeight()>11){
+                fishes.remove(f);
+            }
+        }
+    }
+
+    public void getStatus(){
+        String total = "Fishes in this aquarium:\n";
+        for (Fish f: fishes){
+           total = f.status() + "\n";
+        }
+        System.out.println(total);
+    }
+}
---
+++ abstractsinterfaces/exams/fishtank/Tang.java
@@ -0,0 +1,27 @@
+package exams.fishtank;
+
+public class Tang extends Fish{
+    private boolean shortMemoryLoss;
+    public Tang(){}
+
+public Tang(String name, int weight, String color, boolean memoryLoss){
+        super(name,weight,color);
+        shortMemoryLoss = memoryLoss;
+}
+
+    @Override
+    public void feed(){
+        setWeight(getWeight()+1);
+    }
+
+    @Override
+    public String status(){
+        String previous = super.status();
+        return previous + ", short-term memory loss: " + shortMemoryLoss;
+    }
+
+    public boolean getShortMemoryLoss(){return shortMemoryLoss;}
+    public void setShortMemoryLoss(boolean loss){
+        shortMemoryLoss=loss;
+    }
+}
